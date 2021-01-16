0eff365043e280eb7a5ba1d26ede33ab9b27d8a1
FGL <1440433105@qq.com>
2021-01-16 18:26:33 +0800

    feat

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,17 +1,5 @@
 <template>
-	<view class="container" :style="'height:'+pageHeight +'px'">
-	  <view class="app-header">
-	  	<view class="left-header">
-			<view class="left-header-title">任务管理系统</view>
-			<view class="left-header-text">	管理你的工作，让你的生活更有品质</view>
-		</view>
-		<view class="right-header">
-			<image src="../../static/image/indeximg.png" mode=""></image>
-		</view>
-	  </view>
-	  <view class="container-main">
-	  	
-	  </view>
+	<view class="container">
 	</view>
 </template>
 
@@ -19,17 +7,7 @@
 	export default {
 		data() {
 			return {
-				pageHeight: 0
 			}
-		},
-		onReady: function() {
-			let t = this
-			uni.getSystemInfo({
-				success: function(res) {
-					console.log('pageHeight',res.windowHeight)
-					t.pageHeight = res.windowHeight
-				}
-			})
 		},
 		methods: {
 		}
@@ -38,47 +16,5 @@
 
 <style lang="scss" scoped>
 	.container {
-		height: 100%;
-		width: 100%;
-		.app-header{
-			padding: 10px;
-			height: 100px;
-			background: #2f5bc2;
-			color: #fff;
-			font-size: 12px;
-			text-align: left;
-			display: flex;
-			justify-content: space-between;
-			
-		}
-		.left-header-title{
-			 margin-top: 10px;
-			font-size: 16px;
-			margin-bottom: 20px;
-			position: relative;
-		}
-		.left-header-title:after{
-			content: '';
-			width: 100px;
-			height: 1px;
-			background-color: #fff;
-			position: absolute;
-			bottom: -10px;
-			left: 0;
-		}
-		.left-header-text{
-			font-size: 12px;
-		}
-		.right-header{
-			image{
-				width: 137px;
-				height: 87px;
-			}
-		}
-				
 	}
-	.container-main{
-	  height: calc(100% - 100px)
-	}
-		
 </style>
---
+++ pages/login/login.vue
@@ -0,0 +1,128 @@
+<template>
+    <view class="body" :style="{height: pageHeight+'px'}">
+        <view class="img-wrap">
+			<image src="/static/image/indeximg.png" mode=""></image>
+        </view>
+		<view class="login-warp">
+			<form @submit="formSubmit" @reset="formReset">
+				<view class="login-list user-name">
+					<text class="iconfont icontouxiang"></text>
+					<input class="uni-input" focus placeholder="请输入用户名" />
+				</view>
+				<view class="login-list user-paswd">
+					<text class="iconfont icontouxiang"></text>
+					<input class="uni-input" focus placeholder="请输入密码" />
+					<text class="iconfont icontouxiang pas-eye"></text>
+				</view>
+				<view class="remb-paswd">
+					<checkbox value="cb" color="#333" style="transform:scale(0.5)" />记住密码
+				</view>
+				<view class="btn-warp">
+					<button type="default" @click="">登 录</button>
+				</view>
+				
+			</form>
+		</view>
+    </view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				pageHeight: 0
+			}
+		},
+		
+		onReady: function() {
+		   let t = this
+		   uni.getSystemInfo({
+		    success: function(res) {
+		     console.log('pageHeight',res.windowHeight)
+		     t.pageHeight = res.windowHeight
+		    }
+		   })
+		  },
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.body{
+		background-color: #2f5bc2;
+		overflow: hidden;
+		font: 0.9rem;
+	}
+	.img-wrap{
+		width: 246px;
+		height: 176px;
+		margin:15px auto;
+		overflow: hidden;
+		image{
+			width: 100%;
+			height: 100%;
+		}
+	}
+	.login-warp{
+		width: 90%;
+		background-color: #fff;
+		color: #333;
+		height: auto;
+		border-radius: 8px;
+		margin: 15px auto;
+		overflow: hidden;
+		
+		.login-list{
+			width: 60%;
+			height: 50px;
+			margin: 20px auto 0;
+			line-height: 55px;
+			border-bottom: 1px solid #C0CFE4;
+		}
+		.user-name,.user-paswd{
+			margin-top: 10px;
+			text{
+				float: left;
+			}
+			input{
+				width: calc(100% - 50px);
+				margin: 12px 0 0 8px;
+				float: left;
+				height: 30px;
+				text-align: left;
+				font-size: 0.9rem;
+			}
+			.pas-eye{
+				float: right;
+			}
+		}
+		.user-name{
+			margin-top: 20px;
+		}
+		
+		.remb-paswd{
+			width: 60%;
+			margin:15px auto;
+			color: #333;
+			text-align: left;
+			font-size: 0.9rem;
+			checkbox{
+				transform-origin: 0 50%;
+				width: 20px;
+			}
+		}
+		.btn-warp{
+			width: 60%;
+			margin:35px auto 30px;
+			button{
+				color: #fff;
+				font-size: 1.2rem;
+				background-color: #2f5bc2;
+				border-radius: 50px;
+			}
+		}
+	}
+
+</style>
