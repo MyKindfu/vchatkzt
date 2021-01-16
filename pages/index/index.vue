dfaf4054d6e650a2590ce60d060d1d72b4eae148
wu zhujiang <wzj@mj.com>
2021-01-16 12:36:14 +0800

    '00'

---
+++ pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="container">
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		methods: {
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.container {
+	}
+</style>
